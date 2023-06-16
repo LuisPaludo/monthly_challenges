a5b144f10a9ccd67c39ebf1ce5c5c0f5cdb310ab
Luís Gustavo dos Santos Paludo <lgspaludo@gmail.com>
2023-06-16 19:00:39 -0300

    Permite a entrada de qualquer mês (qualquer texto também)

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge(request, month):
+    if month == 'january':
+        challenge_text = 'Vai correr meu caro!'
+    else:
+        challenge_text = 'Eu confio que você digitou um mês'
+    return HttpResponse(challenge_text)
